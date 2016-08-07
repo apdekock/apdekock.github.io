7552ae53612e159093b834535c9442ca2f2b2e97
Philip de Kock <Philip de Kock>
2016-08-07 19:37:39 +0200

    Signed-off-by: Philip de Kock <Philip de Kock>

--- UpdateWindowsService/ConsoleApplication1/Program.cs
+++ UpdateWindowsService/ConsoleApplication1/Program.cs
@@ -1,37 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using Aggregator;
+using GitSharp;
+using GitSharp.Commands;
+using GitSharp.Core.Transport;
 
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
-
         {
-            args = new[] { @"C:\Dropbox\jnk\WeSellCars" };
+            var arg0 = @"C:\Dropbox\jnk\WeSellCars\"; // file path
+            var arg1 = @"C:\git\apdekock.github.io\"; //repo path
+            var arg2 = @"_posts\2015-08-04-weMineData.markdown"; //post path
+            var arg3 = @"C:\Program Files\Git\cmd\git.exe"; //git path
+
+            args = new[] { arg0, arg1, arg2, arg3 };
 
             StringBuilder postTemplate = new StringBuilder();
             postTemplate.AppendLine("---");
             postTemplate.AppendLine("layout: post ");
             postTemplate.AppendLine("title: \"Scraping\" ");
-            postTemplate.AppendLine("date: {0} ");
+            postTemplate.AppendLine("date: 2016-08-07");
             postTemplate.AppendLine("quote: \"If you get pulled over for speeding.Tell them your spouse has diarrhoea. — Phil Dunphy [Phil’s - osophy]\"");
             postTemplate.AppendLine("categories: scraping, auto generatingpost ");
             postTemplate.AppendLine("---");
-            postTemplate.AppendLine(" This page is a daily re-generated post [last re-generated {0}], that shows the movement of prices on the www.wesellcars.co.za website.");
-            postTemplate.AppendLine("");
-            postTemplate.AppendLine("{1}");
+            //postTemplate.AppendLine("While investigating Git and GitSharp[link]");
+            postTemplate.AppendLine(string.Format("This page is a daily re-generated post [last re-generated  {0}], that shows the movement of prices on the www.wesellcars.co.za website.", DateTime.Now));
             postTemplate.AppendLine("");
 
             var aggregateData = new AggregateData(new FileSystemLocation(args[0]));
             var dictionary = aggregateData.Aggregate();
 
             var html = aggregateData.GetHTML(dictionary);
-            var post = string.Format(postTemplate.ToString(), string.Format("{0}-{1}-{2}", DateTime.Now.Year, DateTime.Now.Month.ToString().PadLeft(2,'0'), DateTime.Now.Day.ToString().PadLeft(2, '0')), html);
+            postTemplate.AppendLine(html);
+
+            // update post file
+            FileInfo fi = new FileInfo(args[1] + args[2]);
+            var streamWriter = fi.CreateText();
+            streamWriter.WriteLine(postTemplate.ToString());
+
+            streamWriter.Flush();
+            streamWriter.Dispose();
+
+
+            Repository repository = new Repository(args[1]);
+
+            repository.Index.Add(args[1] + args[2]);
+            Commit commited = repository.Commit(string.Format("Updated {0}", DateTime.Now), new Author("Philip de Kock", "philipdekock@gmail.com"));
+            if (commited.IsValid)
+            {
+                string gitCommand = args[3];
+                const string gitPushArgument = @"push origin";
+                ProcessStartInfo psi = new ProcessStartInfo(gitCommand, gitPushArgument)
+                {
+                    WorkingDirectory = args[1],
+                    UseShellExecute = true
+                };
+                Process.Start(psi);
+            }
         }
     }
 }
---
+++ UpdateWindowsService/GetGumtree/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GetGumtree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GetGumtree")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e07f5d2-36a8-455b-8e49-3215d6f40d9e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
